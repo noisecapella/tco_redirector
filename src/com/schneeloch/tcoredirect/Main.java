17054c71e652673420a75b988323bc6841f4616f
Grischa Brockhaus <github@brockha.us>
2012-09-08 14:08:24 +0200

    Hooks into https://t.co links now, too. German translation. Import cleanup.

--- src/com/schneeloch/tcoredirect/Main.java
+++ src/com/schneeloch/tcoredirect/Main.java
@@ -1,35 +1,11 @@
 package com.schneeloch.tcoredirect;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URI;
-import java.net.URISyntaxException;
-
-import org.apache.http.Header;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.ProtocolException;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.RedirectHandler;
-import org.apache.http.client.methods.HttpHead;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.client.DefaultRedirectHandler;
-import org.apache.http.protocol.HttpContext;
-
-
-
-import com.schneeloch.tcoredirect.R;
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
-import android.widget.RemoteViews.ActionException;
 
 public class Main extends Activity {
 
@@ -50,7 +26,7 @@ public class Main extends Activity {
 		}
 		else
 		{
-			report("Problem reading url", null);
+			report(getResources().getString(R.string.err_reading_url), null);
 		}
 	}
 
